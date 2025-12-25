8e301369798778c6d6be9dd745e37c0f031cdbbd
azad <azad0xf1@gmail.com>
2025-12-25 15:33:15 +0330

    start working on day-09

---
+++ day-09/part-01/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const FILENAME = "../sample-input.txt"
+
+type Node struct {
+	X int
+	Y int
+}
+
+type Distance struct {
+	Node1    *Node
+	Node2    *Node
+	Distance float64
+}
+
+func parseNodes() ([]Node, error) {
+	file, err := os.Open(FILENAME)
+	if err != nil {
+		println()
+		return nil, errors.New("there was a problem opening file: " + FILENAME)
+	}
+	defer file.Close()
+
+	nodes := make([]Node, 0, 500)
+
+	scanner := bufio.NewScanner(file)
+
+	// parse the cordinates of the nodes from the file
+	for scanner.Scan() {
+		line := scanner.Text()
+		cords := strings.Split(line, ",")
+		if len(cords) != 2 {
+			return nil, errors.New("cords length must be 2. recieved: " + line)
+		}
+		x, err := strconv.Atoi(cords[0])
+		if err != nil {
+			return nil, errors.New("failed to parse the cordinates in this string: " + line)
+		}
+		y, err := strconv.Atoi(cords[1])
+		if err != nil {
+			return nil, errors.New("failed to parse the cordinates in this string: " + line)
+		}
+		node := Node{X: x, Y: y}
+		nodes = append(nodes, node)
+	}
+	return nodes, nil
+}
+
+func calculateDistances(nodes []Node) []Distance {
+	numberOfDistances := len(nodes) * (len(nodes) - 1) / 2
+	distances := make([]Distance, numberOfDistances)
+	index := 0
+	var n1, n2 *Node
+	for i := 0; i < len(nodes)-1; i++ {
+		n1 = &nodes[i]
+		for j := i + 1; j < len(nodes); j++ {
+			n2 = &nodes[j]
+			distances[index] = Distance{
+				Node1:    n1,
+				Node2:    n2,
+				Distance: math.Sqrt(float64((n1.X-n2.X)*(n1.X-n2.X) + (n1.Y-n2.Y)*(n1.Y-n2.Y))),
+			}
+			index++
+		}
+	}
+	return distances
+}
+
+type Circuit []*Node
+
+func main() {
+	nodes, err := parseNodes()
+	if err != nil {
+		println("there was an issue parsing the nodes. error:\n", err)
+		return
+	}
+	distances := calculateDistances(nodes)
+	slices.SortFunc(distances, func(d1, d2 Distance) int {
+		if d1.Distance < d2.Distance {
+			return 1
+		} else if d1.Distance > d2.Distance {
+			return -1
+		}
+		return 0
+	})
+	print(nodes)
+}
