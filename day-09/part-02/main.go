b9a68187185218d212ef657b860fac4dbde93487
circuit <oxcl@proton.me>
2025-12-28 17:54:38 +0330

    solve part-02 of day-09 (finally!)

--- day-09/part-02/main.go
+++ day-09/part-02/main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-const FILENAME = "../sample-input.txt"
+const FILENAME = "../input.txt"
 
 type Node struct {
 	X int
@@ -50,50 +50,17 @@ func parseNodes() ([]Node, error) {
 	return nodes, nil
 }
 
-func reduceOffset(nodes []Node) (offsetX int, offsetY int, reducedNodes []Node) {
-	minX := math.MaxFloat64
-	minY := math.MaxFloat64
-	for _, node := range nodes {
-		minX = math.Min(minX, float64(node.X))
-		minY = math.Min(minY, float64(node.Y))
-	}
-	offsetX = int(minX)
-	offsetY = int(minY)
-	reducedNodes = make([]Node, len(nodes))
-	for index, node := range nodes {
-		reducedNodes[index] = Node{X: node.X - offsetX, Y: node.Y - offsetY}
-	}
-	return offsetX, offsetY, reducedNodes
-}
-
-func compressNodes(nodes []Node) (float64, float64, []Node) {
-	gcdX, gcdY := FindNodesGCD(nodes)
-	if gcdX == 1 && gcdY == 1 {
-		return 1, 1, nodes
-	}
-	newNodes := make([]Node, len(nodes), len(nodes))
-	for index, node := range nodes {
-		newNodes[index] = Node{
-			X: node.X / gcdX,
-			Y: node.Y / gcdY,
-		}
-	}
-	return 1.0 / float64(gcdX), 1.0 / float64(gcdY), newNodes
-}
-
 type Walls struct {
 	XMap   map[int][2]int
 	YMap   map[int][2]int
 	Width  int
 	Height int
-	cache  map[int]bool
 }
 
 func calculateWalls(nodes []Node) Walls {
 	walls := Walls{
-		XMap:  make(map[int][2]int),
-		YMap:  make(map[int][2]int),
-		cache: make(map[int]bool),
+		XMap: make(map[int][2]int),
+		YMap: make(map[int][2]int),
 	}
 	wrappedNodes := append(nodes, nodes[0])
 
@@ -102,77 +69,75 @@ func calculateWalls(nodes []Node) Walls {
 		nextNode := wrappedNodes[i+1]
 		if node.X == nextNode.X {
 			min := int(math.Min(float64(node.Y), float64(nextNode.Y)))
-			max := int(math.Min(float64(node.Y), float64(nextNode.Y)))
+			max := int(math.Max(float64(node.Y), float64(nextNode.Y)))
 			walls.XMap[node.X] = [2]int{min, max}
 		}
 		if node.Y == nextNode.Y {
 			min := int(math.Min(float64(node.X), float64(nextNode.X)))
-			max := int(math.Min(float64(node.X), float64(nextNode.X)))
+			max := int(math.Max(float64(node.X), float64(nextNode.X)))
 			walls.YMap[node.Y] = [2]int{min, max}
 		}
 	}
-	width := 0
-	height := 0
+	maxX := 0
+	maxY := 0
 	for i := 0; i < len(nodes); i++ {
-		width = int(math.Max(float64(nodes[i].X), float64(width)))
-		height = int(math.Max(float64(nodes[i].Y), float64(height)))
+		maxX = int(math.Max(float64(nodes[i].X), float64(maxX)))
+		maxY = int(math.Max(float64(nodes[i].Y), float64(maxY)))
 	}
-	walls.Width = width
-	walls.Height = height
+	walls.Width = maxX + 1
+	walls.Height = maxY + 1
 	return walls
 }
 
-func (walls Walls) isNodeOnWall(point Node) bool {
-	if wallRange, exists := walls.XMap[point.X]; exists {
-		if point.Y >= wallRange[0] && point.Y <= wallRange[1] {
-			return true
-		} else {
-			return false
+func (walls Walls) isNodeInsideWalls(point Node) bool {
+	// check if there's a wall at the top of the point
+	hitWall := false
+	for wallY, xRange := range walls.YMap {
+		wallX1, wallX2 := xRange[0], xRange[1]
+		if wallY <= point.Y && point.X >= wallX1 && point.X <= wallX2 {
+			hitWall = true
+			break
 		}
-	} else if wallRange, exists := walls.YMap[point.Y]; exists {
-		if point.Y >= wallRange[0] && point.Y <= wallRange[1] {
-			return true
-		} else {
-			return false
-		}
-	} else {
+	}
+	if !hitWall {
 		return false
 	}
-}
-
-var directions = [4][2]int{
-	{0, -1},
-	{0, 1},
-	{1, 0},
-	{-1, 0},
-}
-
-func (walls Walls) isNodeInsideWalls(point Node) bool {
-	if cacheHit, exists := walls.cache[point.Y*walls.Width+point.X]; exists {
-		return cacheHit
-	}
-	for _, direction := range directions {
-		dx := direction[0]
-		dy := direction[1]
-		didHitWall := false
-		for x, y := point.X, point.Y; x >= 0 && x <= walls.Width && y >= 0 && y <= walls.Height; x, y = x+dx, y+dy {
-			if cacheHit, exists := walls.cache[y*walls.Width+x]; exists {
-				for cx, cy := point.X, point.Y; cx != x && cy != y; cx, cy = cx+dx, cy+dy {
-					walls.cache[cy*walls.Width+cx] = cacheHit
-				}
-				return cacheHit
-			}
-			if walls.isNodeOnWall(Node{x, y}) {
-				didHitWall = true
-				break
-			}
-		}
-		if !didHitWall {
-			walls.cache[point.Y*walls.Width+point.X] = false
-			return false
+	hitWall = false
+	// check if there's a wall at the bottom of the point
+	for wallY, xRange := range walls.YMap {
+		wallX1, wallX2 := xRange[0], xRange[1]
+		if wallY >= point.Y && point.X >= wallX1 && point.X <= wallX2 {
+			hitWall = true
+			break
 		}
 	}
-	walls.cache[point.Y*walls.Width+point.X] = true
+	if !hitWall {
+		return false
+	}
+	// check if there's a wall on the right of the point
+	hitWall = false
+	for wallX, yRange := range walls.XMap {
+		wallY1, wallY2 := yRange[0], yRange[1]
+		if wallX >= point.X && point.Y >= wallY1 && point.Y <= wallY2 {
+			hitWall = true
+			break
+		}
+	}
+	if !hitWall {
+		return false
+	}
+	hitWall = false
+	// check if there's a wall at the bottom of the point
+	for wallX, yRange := range walls.XMap {
+		wallY1, wallY2 := yRange[0], yRange[1]
+		if wallX <= point.X && point.Y >= wallY1 && point.Y <= wallY2 {
+			hitWall = true
+			break
+		}
+	}
+	if !hitWall {
+		return false
+	}
 	return true
 }
 
@@ -182,7 +147,7 @@ type Area struct {
 	Area  float64
 }
 
-func calculateAreas(nodes []Node, compressX float64, compressY float64) []Area {
+func calculateAreas(nodes []Node) []Area {
 	numberOfAreas := len(nodes) * (len(nodes) - 1) / 2
 	areas := make([]Area, numberOfAreas)
 	index := 0
@@ -191,8 +156,8 @@ func calculateAreas(nodes []Node, compressX float64, compressY float64) []Area {
 		n1 = &nodes[i]
 		for j := i + 1; j < len(nodes); j++ {
 			n2 = &nodes[j]
-			width := (math.Abs(float64(n1.X)-float64(n2.X)) / compressX) + 1
-			height := (math.Abs(float64(n1.Y)-float64(n2.Y)) / compressY) + 1
+			width := math.Abs(float64(n1.X)-float64(n2.X)) + 1
+			height := math.Abs(float64(n1.Y)-float64(n2.Y)) + 1
 			areas[index] = Area{
 				Node1: n1,
 				Node2: n2,
@@ -210,12 +175,9 @@ func main() {
 		println("there was an issue parsing the nodes. error:\n", err)
 		return
 	}
-	_, _, nodes = reduceOffset(nodes)
-	compressX, compressY, nodes := compressNodes(nodes)
+
 	walls := calculateWalls(nodes)
-	// width, height, nodePairs := pairNodes(nodes)
-	// grid := generateGrid(width, height, nodes)
-	areas := calculateAreas(nodes, compressX, compressY)
+	areas := calculateAreas(nodes)
 	slices.SortFunc(areas, func(d1, d2 Area) int {
 		if d1.Area < d2.Area {
 			return 1
@@ -237,41 +199,47 @@ func main() {
 
 		// even if all the four corners are inside the walls we got to still check if no wall is carving inside the borders
 		// meaning that we check to see if all the points on the borders of the rectangle are inside the walls
-		var dx int
-		var dy int
-		if x1 < x2 {
-			dx = 1
-		} else {
-			dx = -1
-		}
-		if y1 < y2 {
-			dy = 1
-		} else {
-			dy = -1
-		}
+		// we do so by checking if any of the borders either have one of their points inside the rectangle
+		// or their range extends the whole size of the rectangle
+		minX := int(math.Min(float64(x1), float64(x2)))
+		minY := int(math.Min(float64(y1), float64(y2)))
+		maxX := int(math.Max(float64(x1), float64(x2)))
+		maxY := int(math.Max(float64(y1), float64(y2)))
 		didFail := false
-		for _, x := range [2]int{x1, x2} {
-			for y := y1; y != y2; y += dy {
-				if !walls.isNodeInsideWalls(Node{x, y}) {
-					didFail = true
-					break
-				}
+		for wallY, xRange := range walls.YMap {
+			wallX1, wallX2 := xRange[0], xRange[1]
+			if wallY <= minY || wallY >= maxY {
+				// the wall is outside of the rectangle. early exit.
+				continue
 			}
-			if didFail {
+			if (wallX1 > minX && wallX1 < maxX) || (wallX2 > minX && wallX2 < maxX) {
+				// this means that one of the edges of a wall is inside the rectangle
+				didFail = true
+				break
+			}
+			if wallX1 <= minX && wallX2 >= maxX {
+				// this means that a wall is bigger than the rectangle and it's going through
+				didFail = true
 				break
 			}
 		}
 		if didFail {
 			continue
 		}
-		for _, y := range [2]int{y1, y2} {
-			for x := x1; y != x2; x += dx {
-				if !walls.isNodeInsideWalls(Node{x, y}) {
-					didFail = true
-					break
-				}
+		for wallX, yRange := range walls.XMap {
+			wallY1, wallY2 := yRange[0], yRange[1]
+			if wallX <= minX || wallX >= maxX {
+				// the wall is outside of the rectangle. early exit.
+				continue
 			}
-			if didFail {
+			if (wallY1 > minY && wallY1 < maxY) || (wallY2 > minY && wallY2 < maxY) {
+				// this means that one of the edges of a wall is inside the rectangle
+				didFail = true
+				break
+			}
+			if wallY1 <= minY && wallY2 >= maxY {
+				// this means that a wall is bigger than the rectangle and it's going through
+				didFail = true
 				break
 			}
 		}
@@ -279,7 +247,8 @@ func main() {
 			continue
 		}
 
-		print("Found it!:", area.Area)
+		println("Found it:", int(area.Area))
+		return
 	}
 	println("couldn't find the largest area!")
 }
